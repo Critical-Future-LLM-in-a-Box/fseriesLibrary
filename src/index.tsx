edce8f9f151d1636854513a624f65331bf672b57
mohamedtharwat000 <mohamed.sarwat.000@gmail.com>
2025-01-07 13:26:08 +0200

    isolate theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,47 +1,69 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import { createRoot } from "react-dom/client";
 
 import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
 
 import { ToastContainer } from "react-toastify";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { AuthWrapper } from "@/components/Auth";
 import App from "@/App";
-
-import { CacheProvider } from "@emotion/react";
-import createEmotionCache from "./createEmotionCache";
-
-const emotionCache = createEmotionCache();
-
-const theme = createTheme();
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 10 * 60 * 1000,
-      refetchInterval: 10 * 60 * 1000
-    },
-    mutations: {}
-  }
-});
+import { AuthWrapper } from "@/components/Auth";
 
 const RootComponent = () => {
+  const theme = useMemo(() => createTheme(), []);
+
+  const queryClient = useMemo(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 10 * 60 * 1000,
+            refetchInterval: 10 * 60 * 1000
+          },
+          mutations: {}
+        }
+      }),
+    []
+  );
+
+  const shadowHost = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (shadowHost.current && !shadowHost.current.shadowRoot) {
+      const shadowRoot = shadowHost.current.attachShadow({ mode: "open" });
+
+      const shadowCache = createCache({
+        key: "mui",
+        container: shadowRoot
+      });
+
+      const root = createRoot(shadowRoot);
+
+      root.render(
+        <CacheProvider value={shadowCache}>
+          <QueryClientProvider client={queryClient}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <ToastContainer />
+              <AuthWrapper>
+                <App />
+              </AuthWrapper>
+            </ThemeProvider>
+          </QueryClientProvider>
+        </CacheProvider>
+      );
+    }
+  }, [theme, queryClient]);
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ToastContainer />
-      <AuthWrapper>
-        <App />
-      </AuthWrapper>
-    </QueryClientProvider>
+    <div
+      style={{ overflow: "clip" }}
+      ref={shadowHost}
+    ></div>
   );
 };
 
-createRoot(document.getElementById("root")!).render(
-  <CacheProvider value={emotionCache}>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RootComponent />
-    </ThemeProvider>
-  </CacheProvider>
-);
+createRoot(document.getElementById("root")!).render(<RootComponent />);
